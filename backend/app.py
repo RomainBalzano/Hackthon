9067cbc6b070f94cd2828fa4dd23076c16069f8e
RomainBalzano <113018203+RomainBalzano@users.noreply.github.com>
2023-12-21 21:40:53 +0100

    v12

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,14 @@
 
 from email.mime.application import MIMEApplication
 from email.mime.text import MIMEText
-import os
 from smtpd import SMTPServer
-import time
-from flask import Flask, request, render_template, redirect, url_for,session,jsonify, send_file
+from flask import Flask, request, render_template, redirect, url_for,session, send_file
 from flask_mail import Mail, Message
 import secrets
 import json
 import pdfkit
 
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.base import MIMEBase
-from email import encoders
+
 
 app = Flask(__name__)
 app.config['MAIL_SERVER']='smtp.gmail.com'
@@ -33,20 +28,23 @@ def index():
 
 @app.route("/home")
 def hello_world():
-    session["username"] = "test"
-    session["nom"]
-    session["score"] = 200
-    return "<p>Hello, World!</p>"
+    return render_template("home.html")
+
+@app.route("/define_user")
+def define_user():
+    #à utiliser sous forme de 
+    # http://127.0.0.1:5000/define_user?name=testnom&firstname=prenom
+    session["username"] = request.args.get('name')
+    session["firstname"] = request.args.get('firstname')
+    session["score"] = 0
+    return redirect("/home")
 
 @app.route("/test")
 def test():
-    msg = Message(subject='Hello from the other side!', sender='hackthonedfgroupe15@gmail.com', recipients=['shinatu1905@gmail.com'])
-    msg.body = "Hey Paul, sending you this email from my Flask app, lmk if it works"
-    with app.open_resource("certificat.pdf") as pdf:
-        msg.attach("certificat.pdf", "application/pdf", pdf.read())
-    
-    mail.send(msg)
-    return "Message sent!"
+    session["username"] = "test"
+    session["firstname"] = "test2"
+    session["score"] = 200
+    return redirect("/home")
     
 
 @app.route("/login", methods=["GET", "POST"])
@@ -83,20 +81,27 @@ def logout():
 
 def profil():
     if not session.get("name"):
-        return redirect("/login")
-    return "<p>Hello, World!</p>"
+        return redirect("/home")
+    return render_template("profile.html")
+
 
 @app.route("/stats")
 def stats():
     if not session.get("name"):
+        return redirect("/home")
+    return render_template("stats.html")
+
+@app.route("/game")
+def game():
+    if not session.get("name"):
         return redirect("/login")
-    return "<p>Hello, World!</p>"
+    return render_template("game.html")
 
 @app.route("/game/board")
 def game_board():
     if not session.get("name"):
         return redirect("/login")
-    return "<p>Hello, World!</p>"
+    return render_template("game_board.html")
 
 @app.route("/game/home" )
 def game_home():
@@ -119,11 +124,22 @@ def game_work():
     with open('./question/home.json') as file:
             data = json.load(file)
     return data
+@app.route("/leaderboard/get")
+def leaderboard_get():
+    with open('./question/leaderboard.json') as file:
+            data = json.load(file)
+    return data
 
+@app.route("/leaderboard/")
+def leaderboard():
+    return render_template("leaderboard.html")
+
+
+    
 @app.route("/certification")
 def download_pdf():
     if not session.get("name") and not session.get("score"):
-        return redirect("/login")
+        return redirect("/home")
     
     if session.get("score")<100:
         return redirect("/home")
@@ -146,30 +162,20 @@ def send_pdf():
     if session.get("score")<100:
         return redirect("/home")
     
+    
     username = session.get("username")
     score = session.get("score")
-    
     config=pdfkit.configuration(wkhtmltopdf='C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe') 
     html_content = f"<h1>Score: {score}</h1><h2>Username: {username}</h2>"
-    
-    pdfkit.from_string(html_content, 'certificat.pdf', configuration=config)
-    
+    pdfkit.from_string(html_content, 'certificat.pdf',configuration=config)
     msg = Message(subject='Eco Certificat de HACKTHON groupe 15', sender='hackthonedfgroupe15@gmail.com', recipients=['shinatu1905@gmail.com'])
-    msg.body = f"Hey {username}, Voici votre eco-certificat !Pour rappel votre score était de {score}"
+    msg.body = f"Hey {username}, Voici votre eco-certificat !Pour rappel votre score etait de {score}"
     with app.open_resource("certificat.pdf") as pdf:
         msg.attach("certificat.pdf", "application/pdf", pdf.read())
     
     mail.send(msg)
     return "Message sent!"
     
-    
-   
-
-
-
-
-  
-
 
 if __name__ == '__main__':
 
